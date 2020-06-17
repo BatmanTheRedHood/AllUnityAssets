6375a316b17e260d9c7d1cdcc9e046c112ff5ab6
Gaurav Kumar <gagautam@amazon.com>
2020-06-17 15:37:10 +0530

    Tanker game change

---
+++ Tanker/Assets/MyScripts/EnemyMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Rigidbody2D rgbody;
+    private Vector2 lookDirection = new Vector2(0, -1);
+
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rgbody = GetComponent<Rigidbody2D>();
+        this.rgbody.velocity = this.lookDirection * this.speed;
+
+        // this.InvokeRandom();
+        StartCoroutine(changeDirection());
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }
+    */
+
+    private void InvokeRandom()
+    {
+        Invoke("changeDirection", 2f);
+    }
+
+    IEnumerator changeDirection()
+    {
+        yield return new WaitForSeconds(5f);
+
+        while (true)
+        {
+            this.setRandomDirection();
+
+            float randomInterval = Random.Range(0f, 10f);
+            //Invoke("InvokeRandom", randomInterval);
+            yield return new WaitForSeconds(randomInterval);
+        }
+    }
+
+    private void setRandomDirection()
+    {
+        int random = Random.Range(0, 4); // 0: UP, 1: DOWN, 2: LEFT, 3: RIGHT
+
+        switch(random)
+        {
+            case 0:
+                this.lookDirection.Set(0, 1);
+                break;
+            case 1:
+                this.lookDirection.Set(0, -1);
+                break;
+            case 2:
+                this.lookDirection.Set(-1, 0);
+                break;
+            case 3:
+                this.lookDirection.Set(1, 0);
+                break;
+        }
+
+        // this.lookDirection.Normalize();
+        this.rgbody.velocity = this.lookDirection * this.speed;
+    }
+}
